bebac9117111d50a506d82304a2abc6c40b80534
joelch97 <125751366+Wirreles@users.noreply.github.com>
2025-01-02 10:38:54 -0300

    crendencialesDeDesarrollo

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import googleCredentials from '../utils/servidoapp-de2a6-805fd5e279c1.json' assert { type: 'json' };
+import googleCredentials from '../utils/pencuentro-8fdb3-2eb07c243f60.json' assert { type: 'json' };
 
 try {
   admin.initializeApp({
@@ -9,15 +9,5 @@ try {
 } catch (error) {
   console.error('Error al inicializar Firebase Admin SDK:', error);
 }
-
-const firestore1 = admin.firestore()
-firestore1.collection('testCollection').add({ test: 'testValue' })
-  .then((docRef) => {
-    console.log('Documento de prueba creado con ID:', docRef.id);
-  })
-  .catch((error) => {
-    console.error('Error al interactuar con Firestore:', error);
-  });
-
 
 export const firestore = admin.firestore();
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,13 +1,12 @@
 export const createProductObject = (data) => {
     return {
-      // photos: data.photos || [] 
+      photos: data.photos || [], 
       title: data.title || '', // Título del producto
       category: data.category || '', // Categoría del producto
       price: data.price || 0, // Precio del producto
       description: data.description || '', // Descripción del producto
       variants: data.variants || [], // Array de variantes
       stock: data.stock || 0, // Stock disponible
-    //   createdAt: new Date().toISOString(), 
     };
   };
   
